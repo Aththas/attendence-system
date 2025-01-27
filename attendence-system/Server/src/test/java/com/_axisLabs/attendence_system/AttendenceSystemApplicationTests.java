de9b606a55407d26f2300df1e2e20bd50a0f1327
aththas <aththasrizwan123@gmail.com>
2025-01-27 15:34:23 +0530

    #1, project initiated with root folder

---
+++ attendence-system/Server/src/test/java/com/_axisLabs/attendence_system/AttendenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com._axisLabs.attendence_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
